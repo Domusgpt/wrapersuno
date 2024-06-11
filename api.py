c0f1faea10b80ef14d0e5e97e8ad72106a7b1125
Domusgpt <Phillips.paul.email@gmail.com>
2024-06-10 21:10:16 -0400

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ import fastapi
 from fastapi.responses import RedirectResponse, JSONResponse
 from suno import __version__
 
-COOKIE = os.getenv("SUNO_COOKIE")
+COOKIE = os.getenv("_cfuvid=OZL4yRvddQoctTjxMAClC02yn_AME178dQPTBdOHQYs-1717435922551-0.0.1.1-604800000; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8yaE5ZTDFCYWpzYjd0cjdOZU5ldVY1VTlGTDEiLCJyb3RhdGluZ190b2tlbiI6IjFmMmw4OGFxMHNzcDA2eXY0cDB4NXZ0ZXFoNDl0bzBuOXBwbTdvdWsifQ.BCsFtC_3uSObK00fjQd0XH6kHpYZ6-e-_yqYlAjWeTU6GdSbvgpyC4bvQtzpnnpXH_oAf4x3OHHsXbEjd8Xt8CC1cufu32769CCBDqtBlmMI4LWp-rPV7bk7lN4lI_bXrqKmqXXJhyqo7TRvuhqdiPscXIqHjZRkPP7iuwntXAUMrlHywr0-FT128hvj-VLGx-CRjolczY1OXxT-ppIT2Jt4SjW-T7xuTpSRnx-PzSiCAfE7_N5yIYAal8ucv2nUKhxQQAcm6IVSyyZ7tV1FRp_CwRyCvdsBBqDIh7OOmUmRynoWy_40Yf8vzvsnlr-5kYHdSmuTUSe7VJYB3rScqw; __client_uat=1717435945; __cf_bm=P33ClypQlY8Q9OimZSjqPcENEQvmJAWdHPQ8oqkycbs-1717440609-1.0.1.1-kwTjBErlbjUnxmzL_Dx4byNW9BBTArx_OwJnzQrG00sLvIx.Oq6C8Y4mwZ6_YKngPwLeKGXbz6rqYpofC0DVAg; mp_26ced217328f4737497bd6ba6641ca1c_mixpanel=%7B%22distinct_id%22%3A%20%22902ef607-3d63-40d2-9038-5bac1f71b263%22%2C%22%24device_id%22%3A%20%2218fdf2875dc6fe-06a59a654f295d-26001c51-e1000-18fdf2875dc6fe%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%2C%22%24user_id%22%3A%20%22902ef607-3d63-40d2-9038-5bac1f71b263%22%7D")
 
 # Initilize Suno API Client
 client = suno.Suno(cookie=COOKIE)
